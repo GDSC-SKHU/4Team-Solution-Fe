2ab5266e36960001a950fb86526c412cd66415c4
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2023-02-08 12:10:08 +0900

    style: google-fonts 적용

--- solution4/src/pages/_app.tsx
+++ solution4/src/pages/_app.tsx
@@ -2,5 +2,9 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <p>인권에 대한 무시와 경멸이 인류의 양심을 격분시키는 만행을 초래하였으며, 인간이 언론과 신앙의 자유, 그리고 공포와</p>
+    </>
+  )
 }
