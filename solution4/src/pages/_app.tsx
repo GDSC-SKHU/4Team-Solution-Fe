51f6545cd295b0fe6d495b6c38b34652d264c687
seulseul_0123 <104067367+yeaseul7@users.noreply.github.com>
2023-02-12 20:21:51 +0900

    build: app 파일 수정(상단바)

--- solution4/src/pages/_app.tsx
+++ solution4/src/pages/_app.tsx
@@ -1,41 +1,96 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 import { useState } from "react";
 import styled from "styled-components";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [NavListState, setNavListState] = useState(true);
+  //메뉴 hover 하면 드러나게 하기 위해서
+  const [SearchDoc, setSearchDoc] = useState(true);
+  const [TestList, setTestList] = useState(true);
+  const [Talkground, setTalkground] = useState(true);
   return (
     <>
       <Headerbox>
         <nav>
           <div>
-            <p
+            <NavTxt
               onMouseOver={() => {
-                setNavListState((prev) => !prev);
-              }}
-              onMouseLeave={() => {
-                setNavListState((prev) => !prev);
+                setSearchDoc((prev) => !prev);
+                setTalkground(true);
+                setTestList(true);
               }}
             >
               상담사 찾기
-            </p>
-            {NavListState ? (
+            </NavTxt>
+            {SearchDoc ? (
               <></>
             ) : (
-              <HideList1>
-                <ul>
-                  <li></li>
-                  <li></li>
-                  <li></li>
-                </ul>
+              <HideList1
+                onMouseLeave={() => {
+                  setSearchDoc((prev) => !prev);
+                }}
+              >
+                <div>
+                  <Link href="/findpsy">
+                    <StyledLink>상담사 보기</StyledLink>
+                  </Link>
+                </div>
+                <div>
+                  <Link href="/HospitalMap">
+                    <StyledLink>병원 모아보기</StyledLink>
+                  </Link>
+                </div>
               </HideList1>
             )}
           </div>
-          <div>자가 분석 테스트</div>
-          <div>소통의 공간</div>
+          <div>
+            <NavTxt
+              onMouseOver={() => {
+                setTestList((prev) => !prev);
+                setTalkground(true);
+                setSearchDoc(true);
+              }}
+            >
+              자가 분석 테스트
+            </NavTxt>
+            {TestList ? (
+              <></>
+            ) : (
+              <HideList1
+                onMouseLeave={() => {
+                  setTestList((prev) => !prev);
+                }}
+              ></HideList1>
+            )}
+          </div>
+          <div>
+            <NavTxt
+              onMouseOver={() => {
+                setTalkground((prev) => !prev);
+                setTestList(true);
+                setSearchDoc(true);
+              }}
+            >
+              소통의 공간
+            </NavTxt>
+            {Talkground ? (
+              <></>
+            ) : (
+              <HideList1
+                onMouseLeave={() => {
+                  setTalkground((prev) => !prev);
+                }}
+              ></HideList1>
+            )}
+          </div>
+        </nav>
+        <nav>
+          <LoginBox>로그인</LoginBox>
+          <LoginBox>회원가입</LoginBox>
         </nav>
       </Headerbox>
+      <Component {...pageProps} />
     </>
   );
 }
@@ -45,29 +100,56 @@ const Headerbox = styled.div`
   width: 100%;
   display: flex;
   align-items: center;
+  justify-content: space-around;
   & > nav > div {
     list-style: none;
     float: left;
     margin: 1rem;
     font-size: 1.2rem;
-    padding: 8px;
-    border-radius: 20px;
+    border-radius: 15px;
     &:hover {
       background-color: #91d196;
-      transition: all 0.5s;
-    }
-    >p{
-      &:hover{
-        color: white;
-        transition: all 0.5s;
-
-      }
     }
   }
 `;
 const HideList1 = styled.div`
-  background-color: #ccff99a0;
+  background-color: #91d196;
   position: fixed;
+  top: 4rem;
+  text-decoration: none;
+  border-radius: 1rem;
+  padding: 1rem;
 
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  & > div {
+    padding: 5px;
+    border-radius: 15px;
+    &:hover {
+      background-color: white;
+      text-decoration: none;
+      transition: all 0.2s;
+    }
+  }
+`;
+const NavTxt = styled.div`
+  height: 2rem;
+  padding: 0.6rem;
+  text-decoration-line: none;
+  &:hover {
+    color: white;
+    transition: all 0.2s;
+  }
+`;
+const StyledLink = styled.a`
+  text-decoration-line: none;
+`;
 
+const LoginBox = styled.div`
+  padding: 10px;
+  &:hover{
+    color: white;
+    transition: all 0.2s;
+  }
 `
