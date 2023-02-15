a6e600d6b5c29041f69ee4220d1ff2f25a7bc3f9
baeksehee <tpgmlqor@gmail.com>
2023-02-15 14:43:47 +0900

    style: 회원 가입 페이지

--- solution4/src/pages/signup.tsx
+++ solution4/src/pages/signup.tsx
@@ -1,3 +1,38 @@
+import Link from "next/link";
+import styled from "styled-components";
+
 export default function signup() {
-  return <></>;
+  return (
+    <Container>
+      <p>회원 가입</p>
+      <Signup>
+        <StyledDiv>일반 회원</StyledDiv>
+        <StyledDiv>
+          <Link href="signupC">상담사</Link>
+        </StyledDiv>
+      </Signup>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const Signup = styled.div`
+  margin-top: 5rem;
+
+  display: flex;
+
+  gap: 1rem;
+`;
+
+const StyledDiv = styled.div`
+  background-color: #ccff99a0;
+  width: 10rem;
+  height: 8rem;
+`;
